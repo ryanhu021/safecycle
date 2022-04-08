91851cef6a134781839f574607a8877fec56ee98
ryyhu <ryanhu021@gmail.com>
2022-04-08 16:31:03 -0700

    Update App.jsx

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -12,7 +12,7 @@ const styles = StyleSheet.create({
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello</Text>
     </View>
   );
 }
