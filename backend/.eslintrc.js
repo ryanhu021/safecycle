5da7db926b2e43f25cf07711b6b8c35acd0c5701
ryyhu <ryanhu021@gmail.com>
2022-04-08 16:27:49 -0700

    Setup frontend linting

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   env: {
-    browser: true,
     commonjs: true,
     es2021: true,
+    node: true,
   },
   extends: ["airbnb-base", "prettier"],
   parserOptions: {
-    ecmaVersion: 13,
+    ecmaVersion: "latest",
   },
   plugins: ["prettier"],
   rules: {
---
+++ frontend/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["plugin:react/recommended", "airbnb", "prettier"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-native", "prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    // suppress errors for missing 'import React' in files
+    "react/react-in-jsx-scope": "off",
+    "no-underscore-dangle": "off",
+  },
+};
