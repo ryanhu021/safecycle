6a3dca40f2893c09710dd2c38e306c6f110e9b9a
vboo123 <89175545+vboo123@users.noreply.github.com>
2022-04-09 16:35:21 -0700

    homepage

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,18 +1,23 @@
-import { StyleSheet, Text, View } from "react-native";
+import { DefaultTheme, Provider as PaperProvider } from "react-native-paper";
+import Home from "./components/home";
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+const theme = {
+  ...DefaultTheme,
+  // Specify custom property
+  myOwnProperty: true,
+  // Specify custom property in nested object
+  colors: {
+    myOwnColor: "#BADA55",
   },
-});
+  textInput: {
+    color: "white",
+  },
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Hellaao</Text>
-    </View>
+    <PaperProvider theme={theme}>
+      <Home />
+    </PaperProvider>
   );
 }
